78d16ee3edf5df8d2c3ab6a5dc96663d1b804518
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2024-05-24 10:01:20 -0500

    ApplyMira

--- Assets/Scripts/CameraInteraction.cs
+++ Assets/Scripts/CameraInteraction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraInteraction : MonoBehaviour
 {
@@ -12,7 +13,6 @@ public class CameraInteraction : MonoBehaviour
     [SerializeField] GameObject ObjectPick;
     [SerializeField] Transform positionInteraction;
     public bool isOnArea;
-
 
     private void Update()
     {
