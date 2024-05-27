e5ffe63df900e3f3e4faf983501e077beec0e01b
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2024-05-27 12:46:49 -0500

    Trying

--- Assets/ScriptableObjects/SoundsSO.cs
+++ Assets/ScriptableObjects/SoundsSO.cs
@@ -11,42 +11,44 @@ public class SoundsSO : ScriptableObject
     [SerializeField] private AudioMixer mixer;
     [SerializeField] private string channelVolume;
     [SerializeField] private float currentVolume;
-
-    private bool ismuted = true;
-    private void Awake()
+    private const string VolumePrefKey = "VolumePref";
+    //private bool ismuted = true;
+    [SerializeField] private float defaultVolume = 1f;
+    private void OnEnable()
     {
-        CargarPreference();
+        LoadPreference();
     }
+
     public void UpdateVolume(Slider slider)
     {
         currentVolume = slider.value;
         mixer.SetFloat(channelVolume, Mathf.Log10(currentVolume) * 20f);
-        SavePreference();
+        SavePreference(); 
     }
 
     private void SavePreference()
     {
-        Debug.Log("Estoy guardando");
-        PlayerPrefs.SetFloat(channelVolume, currentVolume);
+        PlayerPrefs.SetFloat(VolumePrefKey, currentVolume);
         PlayerPrefs.Save();
     }
-    private void CargarPreference()
-    {
-        PlayerPrefs.GetFloat(channelVolume, currentVolume);
-    }
 
-    public void MuteVolume()
+    private void LoadPreference()
     {
-        if (ismuted == true)
-        {
-            mixer.SetFloat(channelVolume, Mathf.Log10(-80) * 20f);
-            ismuted = false;
-        }
-        else
-        {
-            mixer.SetFloat(channelVolume, Mathf.Log10(currentVolume) * 20f);
-            ismuted = true;
-        }
+        currentVolume = PlayerPrefs.GetFloat(VolumePrefKey, defaultVolume);
+        mixer.SetFloat(channelVolume, Mathf.Log10(currentVolume) * 20f);
     }
+    //public void MuteVolume()
+    //{
+    //    if (ismuted == true)
+    //    {
+    //        mixer.SetFloat(channelVolume, Mathf.Log10(-80) * 20f);
+    //        ismuted = false;
+    //    }
+    //    else
+    //    {
+    //        mixer.SetFloat(channelVolume, Mathf.Log10(currentVolume) * 20f);
+    //        ismuted = true;
+    //    }
+    //}
 
 }
